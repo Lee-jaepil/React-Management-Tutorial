cfbb25b7abc32b5dd71f88d0770e08c5f7702fa8
Lee jae-pil <leejaepil6547@gmail.com>
2023-11-16 15:35:24 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer';
 import './App.css';
+
+const customers = [
+{
+  'id': 1,
+  // 'image' : 'https,
+  'name' : '홍길동',
+  'birthday' : '961222',
+  'gender' : '남자',
+  'job' : '대학생'
+},
+{
+  'id': 2,
+  // 'image' : 'https,
+  'name' : '이재필',
+  'birthday' : '931122',
+  'gender' : '남자',
+  'job' : '대학생'
+},
+{
+  'id': 3,
+  // 'image' : 'https,
+  'name' : '노가다',
+  'birthday' : '940912',
+  'gender' : '남자',
+  'job' : '현장직'
+}
+]
 
 class App extends Component {
   render() {
     return (
-      <div className='gray-background'>
-        <img src={logo} lat="logo"></img>
-        <h2>Let's develop management system!</h2>
+      <div>
+        {
+          customers.map(c => {
+            return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              // image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            >
+            </Customer>
+            )
+          })
+        }
       </div>
     );
   }
